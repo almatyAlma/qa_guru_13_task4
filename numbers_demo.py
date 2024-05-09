9e83a868df185313118c711298ada786a649865b
Bekbergenova Alma <alma02071988@gmail.com>
2024-05-09 15:44:26 +0500

    Задание

--- numbers_demo.py
+++ numbers_demo.py
@@ -1,17 +1,34 @@
 a = 123
 a = a + 456
-a = a -456
+a = a - 456
 a = a / 456
 a = a ** 456
 a = a ** 3
 a = (100 + 200) /300 * 400
+
+a = 123456789088776655443332234567
 
 
 
 a = 0.5
 
 print(a)
+
 a = 0.1 + 0.2
 print(a)
-assert  a ++ 0.3
-assert a - 0.2 - 0.3
+# assert a == 0.3
+
+
+ #математика!
+
+import  math
+print(math.pi)
+
+ #Рандом!
+
+import random
+print("---------")
+print(random.randint(a:0, b:100))
+print(random.randint(a:0, b:100))
+
+print("----------")
