87d887bbc8bffc1409c1efed8ee8242e25075144
Bekbergenova Alma <alma02071988@gmail.com>
2024-05-14 14:51:44 +0500

    Задание

---
+++ conditions.py
@@ -0,0 +1,29 @@
+
+# Boolean - 3 состояния
+b = bool
+
+{
+    "key": true
+}
+
+t = True
+f = False
+n = None
+
+# if/elif/else
+
+if True:
+    print("я выполняюсь!")
+
+if not False:
+    print("я никогда не выполняюсь")
+
+    code = "1000"
+
+    if 200 <= code < 400:
+        print("Проверка прошла, хороший ответ!")
+    elif 400 <= code < 600:
+        print("плохой ответ")
+    else:
+        print("какой-то странный код")
+
